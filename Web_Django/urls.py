c7631adb7e89526d4322593aab8f6ff8c69f437a
sergeypesegov <serega.pesegov@yandex.ru>
2020-01-16 19:18:12 +0300

    added a profile and corrected authentification

---
+++ Web_Django/urls.py
@@ -0,0 +1,26 @@
+"""Web_Django URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from users import views as user_views
+
+urlpatterns = [
+    # оставить path('', include('forum.urls') для отображение стартовой страницы
+    path('admin/', admin.site.urls),
+    path('forum/', include('forum.urls')),
+    path('register/', user_views.register, name='register'),
+    path('about/', include('forum.urls')),
+]
